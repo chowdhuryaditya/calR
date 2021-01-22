c7e641757c461f4078f4dcf254ae147d43e58e21
chowdhuryaditya <chowdhury@ncra.tifr.res.in>
2021-01-22 20:39:49 +0530

    singlepol issue with applycal fixed

--- gaincalRIO.py
+++ gaincalRIO.py
@@ -16,11 +16,15 @@ class gaincalR(calRcore.coreIO.calibSolver):
 		#	if(not self.error):
 		#		print("Cannot combine Spectral Windows. Were different windows observed simultaneously?")
 				
-	def initializeGains(self):		
-		self.gains=np.zeros((self.nCorr,1,self.Nant),dtype=np.complex128) 
-		self.gains_er=np.zeros((self.nCorr,1,self.Nant))
-		self.antFlags=np.zeros((self.nCorr,1,self.Nant),dtype=np.bool)
-		
+	def initializeGains(self):	
+		if(self.nCorr==1):	
+			self.gains=np.zeros((2,1,self.Nant),dtype=np.complex128) 
+			self.gains_er=np.zeros((2,1,self.Nant))
+			self.antFlags=np.zeros((2,1,self.Nant),dtype=np.bool)
+		else:
+			self.gains=np.zeros((self.nCorr,1,self.Nant),dtype=np.complex128) 
+			self.gains_er=np.zeros((self.nCorr,1,self.Nant))
+			self.antFlags=np.zeros((self.nCorr,1,self.Nant),dtype=np.bool)
 	
 	def validateSolint(self):
 		nsol=np.sum(self.solintMap,axis=1)
